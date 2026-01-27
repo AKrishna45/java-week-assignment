7696c99d7416d8657422ca11857e1b63e637a937
AKrishna45 <ukdkittu@gmail.com>
2026-01-27 18:33:29 +0530

    first commit

---
+++ task-1.java
@@ -0,0 +1,18 @@
+class Task1 {
+    int EvenOddDigitsSum(int input1, String input2) {
+        int sum = 0;
+        while (input1 > 0) {
+            int d = input1 % 10;
+            if (input2.equals("even") && d % 2 == 0) sum += d;
+            if (input2.equals("odd") && d % 2 != 0) sum += d;
+            input1 /= 10;
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        Task1 task = new Task1();
+        System.out.println(task.EvenOddDigitsSum(12345, "even"));   // Expected: 6
+        System.out.println(task.EvenOddDigitsSum(12345, "odd"));    // Expected: 9
+    }
+}
